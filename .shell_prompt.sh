00a7d3c2da541b3bf96f7e5f0f69c55171b63ef7
Benson Li <bensonbinbinli@gmail.com>
2017-11-12 10:27:46 -0800

    got conda env working for promptline

--- .shell_prompt.sh
+++ .shell_prompt.sh
@@ -22,6 +22,18 @@ function __promptline_ps1 {
   # section "a" slices
   __promptline_wrapper "$(__promptline_vcs_branch)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
 
+  # section "x" header
+  slice_prefix="${x_bg}${sep}${x_fg}${x_bg}${space}" slice_suffix="$space${x_sep_fg}" slice_joiner="${x_fg}${x_bg}${alt_sep}${space}" slice_empty_prefix="${x_fg}${x_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "x" slices
+  __promptline_wrapper "$(__promptline_git_status)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
+  # section "y" header
+  slice_prefix="${y_bg}${sep}${y_fg}${y_bg}${space}" slice_suffix="$space${y_sep_fg}" slice_joiner="${y_fg}${y_bg}${alt_sep}${space}" slice_empty_prefix="${y_fg}${y_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "y" slices
+  __promptline_wrapper "$CONDA_DEFAULT_ENV" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
   # close sections
   printf "%s" "${reset_bg}${sep}$reset$space"
 }
@@ -90,6 +102,18 @@ function __promptline_left_prompt {
   # section "a" slices
   __promptline_wrapper "$(__promptline_vcs_branch)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
 
+  # section "x" header
+  slice_prefix="${x_bg}${sep}${x_fg}${x_bg}${space}" slice_suffix="$space${x_sep_fg}" slice_joiner="${x_fg}${x_bg}${alt_sep}${space}" slice_empty_prefix="${x_fg}${x_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "x" slices
+  __promptline_wrapper "$(__promptline_git_status)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
+  # section "y" header
+  slice_prefix="${y_bg}${sep}${y_fg}${y_bg}${space}" slice_suffix="$space${y_sep_fg}" slice_joiner="${y_fg}${y_bg}${alt_sep}${space}" slice_empty_prefix="${y_fg}${y_bg}${space}"
+  [ $is_prompt_empty -eq 1 ] && slice_prefix="$slice_empty_prefix"
+  # section "y" slices
+  __promptline_wrapper "$CONDA_DEFAULT_ENV" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; is_prompt_empty=0; }
+
   # close sections
   printf "%s" "${reset_bg}${sep}$reset$space"
 }
@@ -100,8 +124,70 @@ function __promptline_wrapper {
   [[ -n "$1" ]] || return 1
   printf "%s" "${2}${1}${3}"
 }
+function __promptline_git_status {
+  [[ $(git rev-parse --is-inside-work-tree 2>/dev/null) == true ]] || return 1
+
+  local added_symbol="●"
+  local unmerged_symbol="✗"
+  local modified_symbol="+"
+  local clean_symbol="✔"
+  local has_untracked_files_symbol="…"
+
+  local ahead_symbol="↑"
+  local behind_symbol="↓"
+
+  local unmerged_count=0 modified_count=0 has_untracked_files=0 added_count=0 is_clean=""
+
+  set -- $(git rev-list --left-right --count @{upstream}...HEAD 2>/dev/null)
+  local behind_count=$1
+  local ahead_count=$2
+
+  # Added (A), Copied (C), Deleted (D), Modified (M), Renamed (R), changed (T), Unmerged (U), Unknown (X), Broken (B)
+  while read line; do
+    case "$line" in
+      M*) modified_count=$(( $modified_count + 1 )) ;;
+      U*) unmerged_count=$(( $unmerged_count + 1 )) ;;
+    esac
+  done < <(git diff --name-status)
+
+  while read line; do
+    case "$line" in
+      *) added_count=$(( $added_count + 1 )) ;;
+    esac
+  done < <(git diff --name-status --cached)
+
+  if [ -n "$(git ls-files --others --exclude-standard)" ]; then
+    has_untracked_files=1
+  fi
+
+  if [ $(( unmerged_count + modified_count + has_untracked_files + added_count )) -eq 0 ]; then
+    is_clean=1
+  fi
+
+  local leading_whitespace=""
+  [[ $ahead_count -gt 0 ]]         && { printf "%s" "$leading_whitespace$ahead_symbol$ahead_count"; leading_whitespace=" "; }
+  [[ $behind_count -gt 0 ]]        && { printf "%s" "$leading_whitespace$behind_symbol$behind_count"; leading_whitespace=" "; }
+  [[ $modified_count -gt 0 ]]      && { printf "%s" "$leading_whitespace$modified_symbol$modified_count"; leading_whitespace=" "; }
+  [[ $unmerged_count -gt 0 ]]      && { printf "%s" "$leading_whitespace$unmerged_symbol$unmerged_count"; leading_whitespace=" "; }
+  [[ $added_count -gt 0 ]]         && { printf "%s" "$leading_whitespace$added_symbol$added_count"; leading_whitespace=" "; }
+  [[ $has_untracked_files -gt 0 ]] && { printf "%s" "$leading_whitespace$has_untracked_files_symbol"; leading_whitespace=" "; }
+  [[ $is_clean -gt 0 ]]            && { printf "%s" "$leading_whitespace$clean_symbol"; leading_whitespace=" "; }
+}
 function __promptline_right_prompt {
-  return
+  local slice_prefix slice_empty_prefix slice_joiner slice_suffix
+
+  # section "x" header
+  slice_prefix="${x_sep_fg}${rsep}${x_fg}${x_bg}${space}" slice_suffix="$space${x_sep_fg}" slice_joiner="${x_fg}${x_bg}${alt_rsep}${space}" slice_empty_prefix=""
+  # section "x" slices
+  __promptline_wrapper "$(__promptline_git_status)" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; }
+
+  # section "y" header
+  slice_prefix="${y_sep_fg}${rsep}${y_fg}${y_bg}${space}" slice_suffix="$space${y_sep_fg}" slice_joiner="${y_fg}${y_bg}${alt_rsep}${space}" slice_empty_prefix=""
+  # section "y" slices
+  __promptline_wrapper "$CONDA_DEFAULT_ENV" "$slice_prefix" "$slice_suffix" && { slice_prefix="$slice_joiner"; }
+
+  # close sections
+  printf "%s" "$reset"
 }
 function __promptline {
   local last_exit_code="${PROMPTLINE_LAST_EXIT_CODE:-$?}"
@@ -131,6 +217,12 @@ function __promptline {
   local c_fg="${wrap}38;5;245${end_wrap}"
   local c_bg="${wrap}48;5;234${end_wrap}"
   local c_sep_fg="${wrap}38;5;234${end_wrap}"
+  local x_fg="${wrap}38;5;245${end_wrap}"
+  local x_bg="${wrap}48;5;234${end_wrap}"
+  local x_sep_fg="${wrap}38;5;234${end_wrap}"
+  local y_fg="${wrap}38;5;250${end_wrap}"
+  local y_bg="${wrap}48;5;238${end_wrap}"
+  local y_sep_fg="${wrap}38;5;238${end_wrap}"
   if [[ -n ${ZSH_VERSION-} ]]; then
     PROMPT="$(__promptline_left_prompt)"
     RPROMPT="$(__promptline_right_prompt)"
