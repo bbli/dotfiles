80803414474351ba338a5ac19eed6f11a8decd96
Benson Li <bensonbinbinli@gmail.com>
2018-01-28 19:04:13 -0800

    Added setup script and .inputrc for bash selective command history

--- init.vim
+++ init.vim
@@ -1,34 +1,4 @@
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 
-if &compatible
-  set nocompatible
-endif
-set runtimepath+=~/.local/share/dein/repos/github.com/Shougo/dein.vim/
-
-"call dein#begin('~/.nvim/plugged')
-"
-"call dein#add('Shougo/deoplete.nvim')
-"call dein#add('simnalamburt/vim-mundo')
-""call dein#add('bfredl/nvim-ipy')
-"call dein#add('vimlab/split-term.vim')
-"call dein#add('zchee/deoplete-jedi')
-"
-"call dein#end()
-"call dein#save_state()
-
-
 source ~/.vimrc
-
-tnoremap ;c <C-\><C-n>
-cnoremap SH terminal<CR>
-
-
-let $NVIM_TUI_ENABLE_TRUE_COLOR=1
-"if (has("termguicolors"))
-" set termguicolors
-"endif
-
-"colorscheme OceanicNext
-
-let g:deoplete#enable_at_startup=1
---
+++ vim_setup.sh
@@ -0,0 +1,27 @@
+# I will manually installed plugins
+
+## This part installs the nesscary software
+apt update 
+apt --assume-yes install vim
+# For clipboard support
+apt --assume-yes install vim-gtk
+apt --assume-yes install git
+apt --assume-yes install ctags
+apt --assume-yes install silversearcher-ag
+apt --assume-yes install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+
+
+# This script will mostly just set up symbolic links
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.inputrc ~/.inputrc
+# ubuntu image already has a bashrc that sources the .bash_aliases file
+# ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.shell_prompt.sh ~/.shell_prompt.sh
+
+## If neovim enabled
+#ln -s ~/dotfiles/init.vim ~/.config/nvim/init.vim
+
